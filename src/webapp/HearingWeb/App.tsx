8f1113a299b30624cc9c53f6ac9ba12a38ac700f
TempusEstNunc <49039565+TempusEstNunc@users.noreply.github.com>
2022-01-09 15:04:36 -0600

    Added tts functionality to home page, can be moved later as needed

--- src/webapp/HearingWeb/App.tsx
+++ src/webapp/HearingWeb/App.tsx
@@ -1,16 +1,22 @@
 import { table } from "console";
 import { Header } from "./components/Header";
+import { TextboxButtonsTTS } from "./components/TextboxButtonsTTS";
 
 declare var require: any
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+function click() {
+    alert('Clicked');
+}
+
 export class Start extends React.Component {
     render() {
         return (
             <div id="root">
-                <Header style={{width:"100%"}}/>
+                <Header style={{ width: "100%" }} />
+                <TextboxButtonsTTS/>
             </div>
                 
         );
