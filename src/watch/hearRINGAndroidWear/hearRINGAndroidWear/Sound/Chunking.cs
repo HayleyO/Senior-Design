25f160ddbb03c65bd78e7ae537d7195facdcc493
Hayley Owens <hayleyjeanowens@gmail.com>
2022-02-10 15:08:31 -0600

    UI Progressbar Element adjusts for sound

---
+++ src/watch/hearRINGAndroidWear/hearRINGAndroidWear/Sound/Chunking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace hearRINGAndroidWear.Sound
+{
+    public class Chunking
+    {
+        public static Dictionary<string, double> SoundLevels = new Dictionary<string, Double>()
+        {
+            {"Green", 50.0},
+            {"Yellow", 90.0 },
+            {"Red", 200.0}
+        };
+    }
+}
--- src/watch/hearRINGAndroidWear/hearRINGAndroidWear/Sound/Record.cs
+++ src/watch/hearRINGAndroidWear/hearRINGAndroidWear/Sound/Record.cs
@@ -1,16 +1,8 @@
-﻿using Android.App;
-using Android.Content;
-using Android.Media;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.Media;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace hearRINGAndroidWear.Sound
 {
@@ -53,6 +45,7 @@ namespace hearRINGAndroidWear.Sound
 					//Divide the sum of squares by the total length of the data to get the volume.
 					double mean = v / (double)numBytes;
 					double volume = 10 * Math.Log10(mean);
+                    MessagingCenter.Send(new Record(), "Volume", volume); //Kind of hacky to enter the subscription, but....
 					Console.WriteLine(volume);	
 				}
 				catch (Exception ex)
