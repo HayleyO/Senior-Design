28c176c3d2e5e7ef5190b8ec7647c35a65a2d4b8
Hayley <hayleyjeanowens@gmail.com>
2022-02-14 10:26:28 -0600

    New app file

---
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hearRingiOSWatchOS WatchKit Extension
+//
+//  Created by Hayley Owens on 2/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
