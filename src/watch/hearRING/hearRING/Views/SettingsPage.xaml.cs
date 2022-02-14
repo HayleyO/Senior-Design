f9f46b59ed872414a7c7107475c1d50e6d8944d9
unknown <eddieredmann3@gmail.com>
2022-02-13 19:12:32 -0600

    Setting up the framework for the Settings Page

---
+++ src/watch/hearRING/hearRING/Views/SettingsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using hearRING.Models;
+using hearRING.Views;
+using hearRING.ViewModels;
+namespace hearRING.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class SettingsPage : ContentPage
+    {
+        public void SettingsPageInit()
+        {
+            InitializeComponent();
+        }
+    }
+}
