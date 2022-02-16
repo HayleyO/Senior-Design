7a2d171ac88be1f074754c8682dcad596a127367
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-02-16 11:28:36 -0600

    Tested on physical watch :)

--- src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Vibration/Vibration.swift
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Vibration/Vibration.swift
@@ -9,14 +9,19 @@ import Foundation
 import WatchKit
 
 class Vibration: NSObject{
+    
+    // vibrate according to decibel level
     func vibrateOnSound(volume: Float) {
         if volume > 90.0 {
             WKInterfaceDevice.current().play(.notification)
-            print("big vibrate")
         }
         else if volume > 50.0 {
-            WKInterfaceDevice.current().play(.click)
-            print("small vibrate")
+            WKInterfaceDevice.current().play(.navigationGenericManeuver)
         }
+    }
+    
+    // vibrate for alarms 
+    func vibrateAlarm(){
+        WKInterfaceDevice.current().play(.notification)
     }
 }
