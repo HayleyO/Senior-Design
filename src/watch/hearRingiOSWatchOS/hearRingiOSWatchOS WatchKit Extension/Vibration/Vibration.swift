67d210bc3de9cbe97c24d9b28321e8705c91e3be
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-03-11 14:55:51 -0600

    fixed bundle identifier issue and added functionality for vibration only on threshold changes and decibel changes greater than 10 decibels

--- src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Vibration/Vibration.swift
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Vibration/Vibration.swift
@@ -10,18 +10,36 @@ import WatchKit
 
 class Vibration: NSObject{
     
+    //tracks whether the change in decibel level is significant enough for a vibration
+    var storedDecibel: Float = 0.0
+    
     // vibrate according to decibel level
+    // if a threshold is hit, vibrate
+    // if the decibel level changes by 10 within the threshold, vibrate
     func vibrateOnSound(volume: Float) {
-        if volume > 90.0 {
-            WKInterfaceDevice.current().play(.notification)
+            if volume > 90.0 {
+                if(storedDecibel < 90.0 || abs(storedDecibel - volume) > 10.0){
+                    WKInterfaceDevice.current().play(.notification)
+                }
+            }
+            else if volume > 50.0 {
+                if(storedDecibel < 50.0 || abs(storedDecibel - volume) > 10.0){
+                    WKInterfaceDevice.current().play(.directionUp)
+                }
         }
-        else if volume > 50.0 {
-            WKInterfaceDevice.current().play(.notification)
-        }
+        storedDecibel = volume
     }
     
     // vibrate for alarms 
     func vibrateAlarm(){
         WKInterfaceDevice.current().play(.notification)
+    }
+    
+    func vibrateLow(){
+        
+    }
+    
+    func vibrateHigh(){
+        
     }
 }
