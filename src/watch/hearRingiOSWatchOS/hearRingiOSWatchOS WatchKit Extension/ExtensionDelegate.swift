a65a61d3c816436ed67b04c807bb7020f87af632
Hayley <hayleyjeanowens@gmail.com>
2022-03-14 17:54:15 -0500

    FINALLY WORKS ON EVERYTHING

--- src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/ExtensionDelegate.swift
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/ExtensionDelegate.swift
@@ -22,6 +22,7 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate{
         alarm.deployAlarm()
     }
     
+   
     
     // what happens when the notification center recieves certain responses for notifications
     // just using this for snooze at the moment
