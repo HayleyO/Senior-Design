53edaa9bad92dc6c0e37f70253e4a998a164f2a2
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-03-14 16:09:44 -0500

    backing out changes

--- src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS/MainView.swift
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS/MainView.swift
@@ -10,30 +10,24 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         TabView {
-            var sttQueue = DispatchQueue(label: "Hearring.SerialQueue")
-            var mainAppQueue = DispatchQueue(label: "Hearring.SerialQueue")
-            
-            sttQueue.async {
-                ContentView()
-                    .tabItem {
-                        Label("Listen", systemImage: "ear.and.waveform")
-                    }
-            }
-            
-            mainAppQueue.async {
-                AlarmView()
-                    .tabItem {
-                        Label("Alarm", systemImage: "alarm")
-                    }
+            ContentView()
+                .tabItem {
+                    Label("Listen", systemImage: "ear.and.waveform")
+                }
 
-                SettingsView()
-                    .tabItem {
-                        Label("Settings", systemImage: "slider.horizontal.3")
-                    }
+            AlarmView()
+                .tabItem {
+                    Label("Alarm", systemImage: "alarm")
+                }
+
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "slider.horizontal.3")
+                }
             }
         }
     }
-}
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
