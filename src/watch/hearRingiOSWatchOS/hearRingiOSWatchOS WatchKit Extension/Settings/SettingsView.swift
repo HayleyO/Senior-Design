729f9b566bb919f06a2cb18b188c3675b98bb0da
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-03-18 15:00:12 -0500

    making a separate function for sending the slider values to core data, and got pulling from core data working

--- src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Settings/SettingsView.swift
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Settings/SettingsView.swift
@@ -6,28 +6,52 @@
 //
 
 import SwiftUI
+import CoreData
 
-struct SettingsView: View {
-    @StateObject var settingsModel = SettingsModel()
-    @State var lowThreshold = 50.0
-    @State var highThreshold = 90.0
+struct SettingsView: View{
+    @Environment(\.managedObjectContext) var moc
+    @State var lowThreshold: Double = 50.0
+    @State var highThreshold: Double = 90.0
     
     var body: some View {
+        let sliderThreshold = ThresholdEntity(context: moc)
         VStack{
-            Text("Low Threshold: \(Int(round(lowThreshold)))")
-        Slider(value: $lowThreshold,
-               in: 0...highThreshold,
-               onEditingChanged: {_ in settingsModel.lowThreshold = lowThreshold})
+            Text("Low Threshold: \(Int(round(sliderThreshold.weakValue)))")
+            Slider(value: $lowThreshold,
+                   in: 0.0...sliderThreshold.weakValue, step:10)
                 .accentColor(.yellow)
-            Text("High Threshold: \(Int(round(highThreshold)))")
+                .onChange(of: lowThreshold){
+                    newThreshold in sliderChanged(progress: newThreshold, slider: sliders.low)
+                }
+            Text("High Threshold: \(Int(round(sliderThreshold.strongValue)))")
         Slider(value: $highThreshold,
-               in: lowThreshold...120,
-               onEditingChanged: {_ in settingsModel.highThreshold = highThreshold})
+               in: sliderThreshold.weakValue...120.0, step: 10)
                 .accentColor(.red)
+                .onChange(of: highThreshold){
+                    newThreshold in sliderChanged(progress: newThreshold, slider: sliders.high)
+                }
         }
-        
     }
+    
+    func sliderChanged(progress: Double, slider: sliders){
+        print("slider value changed to \(progress)")
+        let sliderThreshold = ThresholdEntity(context: moc)
+        if(slider == sliders.low){
+            sliderThreshold.setValue(progress, forKey: "weakValue")
+            try? moc.save()
+        }
+        else if(slider == sliders.high){
+            sliderThreshold.setValue(progress, forKey: "strongValue")
+        }
+    }
+        
 }
+
+enum sliders{
+    case low
+    case high
+}
+
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
