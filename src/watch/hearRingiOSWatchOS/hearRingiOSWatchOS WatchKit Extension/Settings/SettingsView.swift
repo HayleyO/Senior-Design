529c4a6efd364f03fcd95a0b51dc403e8a1f37ba
tdog989 <49039855+tdog989@users.noreply.github.com>
2022-03-29 17:03:55 -0500

    Removed the step specification from the sliders on the apple watch. Step was creating the fatal error as, when using step, the slider must always be able to be adjusted in at least one direction. Due to this, the watch app would crash when one of the two sliders was unable to be adjusted.

--- src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Settings/SettingsView.swift
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Settings/SettingsView.swift
@@ -12,6 +12,7 @@ struct SettingsView: View{
     @Environment(\.managedObjectContext) var moc
     @State var lowThreshold: Double = 50.0
     @State var highThreshold: Double = 90.0
+    @State var thresholdBuffer: Double = 10.0
     @StateObject var shared = Connectivity.shared
     @StateObject var controller = DataController()
     @State var settings: ThresholdEntity = ThresholdEntity()
@@ -20,14 +21,14 @@ struct SettingsView: View{
         VStack{
             Text("Low Threshold: \(Int(round(lowThreshold)))")
             Slider(value: $lowThreshold,
-                   in: 0.0...highThreshold, step:10)
+                   in: 0.0...highThreshold-thresholdBuffer)
                 .accentColor(.yellow)
                 .onChange(of: lowThreshold){
                     newThreshold in sliderChanged(value: newThreshold, slider: sliders.low)
                 }
             Text("High Threshold: \(Int(round(highThreshold)))")
             Slider(value: $highThreshold,
-               in: lowThreshold...120.0, step: 10)
+                   in: lowThreshold+thresholdBuffer...120.0)
                 .accentColor(.red)
                 .onChange(of: highThreshold){
                     newThreshold in sliderChanged(value: newThreshold, slider: sliders.high)
@@ -43,12 +44,12 @@ struct SettingsView: View{
     func sliderChanged(value: Double, slider: sliders){
         print("slider value changed to \(value)")
         if(slider == sliders.low){
-            controller.updateSettings(buffer: 0.1, weak: value, strong: highThreshold)
-            shared.send(bufferValue: 0.1, strongValue: highThreshold, weakValue: value, delivery: .highPriority)
+            controller.updateSettings(buffer: 10, weak: value, strong: highThreshold)
+            shared.send(bufferValue: 10, strongValue: highThreshold, weakValue: value, delivery: .highPriority)
         }
         else if(slider == sliders.high){
-            controller.updateSettings(buffer: 0.1, weak: lowThreshold, strong: value)
-            shared.send(bufferValue: 0.1, strongValue: value, weakValue: lowThreshold, delivery: .highPriority)
+            controller.updateSettings(buffer: 10, weak: lowThreshold, strong: value)
+            shared.send(bufferValue: 10, strongValue: value, weakValue: lowThreshold, delivery: .highPriority)
         }
     }
         
