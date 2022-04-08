eeba67123912b0b929b7f6e8fc33c1134d4d659e
Ashley Palmer <ashley-palmer@suddenlink.net>
2022-04-08 14:59:01 -0500

    edit button created, crash-ish bug on second edit

---
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS/Settings/PresetEdit.swift
@@ -0,0 +1,20 @@
+//
+//  PresetEdit.swift
+//  hearRingiOSWatchOS
+//
+//  Created by Ashley Palmer on 4/8/22.
+//
+
+import SwiftUI
+
+struct PresetEdit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PresetEdit_Previews: PreviewProvider {
+    static var previews: some View {
+        PresetEdit()
+    }
+}
