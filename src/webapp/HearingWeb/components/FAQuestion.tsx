35466c54924cdb092670b04ebf72e60f184c316d
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-04-13 14:39:31 -0500

    Got dynamic text changing working onclick for the first question

--- src/webapp/HearingWeb/components/FAQuestion.tsx
+++ src/webapp/HearingWeb/components/FAQuestion.tsx
@@ -1,26 +1,36 @@
 import { colors } from "../colors"
 import "../styles.css"
-
+import React from "react"
 declare var require: any
 var React = require('react');
 
 export const FAQuestion = (props) => {
-        return(
+    return (
+            <div>
         <div className="pagePanel">
                 <table style={{width:"100%"}}>
             <tr>
                 <td>
                    <div className="subHeader">{props.header}</div>
                         </td>
-                        <td style={{ textAlign: "right", border: "none", backgroundColor: "azure" }} onClick={open(props.body)}><button>v</button></td>
-                </tr>
+                        <td style={{ textAlign: "right", borderColor: "azure", backgroundColor: "azure" }} id="button" onClick={(e) => open(e, props.body)}><button>v</button></td>
+                        </tr>
                 </table>
+                <div id="answer" className="indentedBodyText" ></div>
+
+            </div>
+            <script type="text/javascript">
+               
+            </script>
             </div>
             )
 }
 
-function open(text) {
-    return (
-        <div className="indentedBodyText">{text}</div>
-    );
+const open = (event: React.MouseEvent<HTMLButtonElement>, body) => {
+    var ans = document.getElementById("answer")
+
+    if (ans != null) {
+        ans.innerHTML = body
+    }
 }
+
--- src/webapp/HearingWeb/navigation/FAQ.tsx
+++ src/webapp/HearingWeb/navigation/FAQ.tsx
@@ -12,7 +12,12 @@ function FAQ() {
         <div className="pageBackground" style={{ backgroundColor: colors.headertab3 }}>
             <Header style={{ width: "100%" }} />
             <div className="pageHeader">Frequently Asked Questions</div>
-            <FAQuestion header={"What does HearRing do with my data?"}/>
+            <FAQuestion
+                header={"What does HearRing do with my data?"}
+                body={"HearRING doesn't store any of your data. All your information stays on your device, and all recording data is deleted after one minute. Sound recordings are only used to provide transcripts and vibrations."} />
+            <FAQuestion
+                header={"Is there a HearRING Android app?"}
+                body={"Unfortunately, there is no Android version of HearRING. Android smartwatches don't have the microphone capability needed for our recording."} />
         </div>
     );
 }
