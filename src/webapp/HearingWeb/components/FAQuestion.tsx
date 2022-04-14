b031cd64ca8611774088ec8a6ea0f5645874154e
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-04-14 10:45:09 -0500

    fixed header and added questions

--- src/webapp/HearingWeb/components/FAQuestion.tsx
+++ src/webapp/HearingWeb/components/FAQuestion.tsx
@@ -6,34 +6,29 @@ var React = require('react');
 
 export function FAQuestion(props){
     return (
-            <div>
         <div className="pagePanel">
-                <table style={{width:"100%"}}>
-            <tr>
-                <td>
-                   <div className="subHeader">{props.header}</div>
-                        </td>
-                        <td style={{ textAlign: "right", borderColor: "azure", backgroundColor: "azure" }} id="button" onClick={(e) => open(e, props.body, props.qid)}><button>v</button></td>
-                        </tr>
-                </table>
-                <div name="answer" className="indentedBodyText" ></div>
+            <table style={{width:"100%"}}>
+                <tr>
+                    <td><div className="subHeader">{props.header}</div></td>
+                    <td className="button" style={{ textAlign: "right" }}  name="button" onClick={(e) => open(e, props.body, props.qid)}><button>view</button></td>
+                </tr>
+            </table>
+            <div name="answer" className="indentedBodyText" ></div>
 
-            </div>
-            <script type="text/javascript">
-               
-            </script>
-            </div>
-            )
+            </div>)
 }
 
 const open = (event: React.MouseEvent<HTMLButtonElement>, body, qid) => {
     var ans = document.getElementsByName("answer")[qid]
+    var button = document.getElementsByName("button")[qid]
 
     if (ans.innerHTML == "") {
         ans.innerHTML = body
+        button.innerHTML = "hide"
     }
     else {
         ans.innerHTML = ""
+        button.innerHTML = "view"
     }
 }
 
--- src/webapp/HearingWeb/components/Header.tsx
+++ src/webapp/HearingWeb/components/Header.tsx
@@ -17,9 +17,10 @@ export const Header = () => {
                         <td >
                             <img src={require('../images/hearringtransparent.png').default} alt="heaRING logo" style={{ width: "200"}}></img>
                         </td>
-                    <td className="applyFont">Slogan</td>
-                    <td></td>
-                    </tr>
+                        <td className="applyFont">Slogan</td>
+                </tr>
+                </table>
+                <table className="headerTable">
                     <tr style={{ height: "40" }}>
                         <td onClick={() => navigate('/', {replace: true})} className="headerTab" style={{ backgroundColor: colors.headertab1 }}>About HearRING</td>
                         <td onClick={() => navigate('/faq', {replace:true})} className="headerTab" style={{ backgroundColor: colors.headertab3 }}>FAQ</td>
