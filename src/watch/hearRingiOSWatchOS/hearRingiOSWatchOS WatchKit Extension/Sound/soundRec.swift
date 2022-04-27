976de2eacf4c3fd74822aebc29e5392e1b3d82f5
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-04-27 16:37:04 -0500

    IT WORKS

---
+++ src/watch/hearRingiOSWatchOS/hearRingiOSWatchOS WatchKit Extension/Sound/soundRec.swift
@@ -0,0 +1,49 @@
+//
+//  soundRecogntion.swift
+//
+//  Created by eddieredmann3 on 4/4/22.
+//
+
+import AVFoundation
+import Foundation
+import SoundAnalysis
+import SwiftUI
+import AVFAudio
+
+class soundRecognizer : NSObject {
+    let audioEngine: AVAudioEngine = AVAudioEngine()
+    let inputBus: AVAudioNodeBus = AVAudioNodeBus(0)
+    var inputFormat: AVAudioFormat!
+    var streamAnalyzer: SNAudioStreamAnalyzer!
+    let sound = SoundResultsObserver()
+    
+    override init() {
+        super.init()
+        
+        inputFormat = audioEngine.inputNode.inputFormat(forBus: inputBus)
+        
+        do {
+            try audioEngine.start()
+
+            audioEngine.inputNode.installTap(onBus: inputBus, bufferSize: 8192, format: inputFormat, block: analyzeAudio(buffer:at:))
+
+            streamAnalyzer = SNAudioStreamAnalyzer(format: inputFormat)
+
+            let request = try SNClassifySoundRequest(classifierIdentifier: SNClassifierIdentifier.version1)
+
+            try streamAnalyzer.add(request, withObserver: sound)
+
+
+        } catch {
+            print("Unable to start AVAudioEngine: \(error.localizedDescription)")
+        }
+    }
+    
+    func analyzeAudio(buffer: AVAudioBuffer, at time: AVAudioTime) {
+        DispatchQueue.main.async {
+            return self.streamAnalyzer.analyze(buffer,
+                                        atAudioFramePosition: time.sampleTime)
+            
+        }
+    }
+}
---
+++ src/watch/hearRingiOSWatchOS/outputListener.swift
@@ -0,0 +1,8 @@
+//
+//  outputListener.swift
+//  hearRingiOSWatchOS
+//
+//  Created by Hannah Folkertsma on 4/27/22.
+//
+
+import Foundation
